99eafb7b44cb4b8a0f11656565f8cecb603af2d1
Shorttbread <luisoliver6@gmail.com>
2025-04-24 03:36:21 +0100

    locked size of frame and trackpanel & button positioned correctly

--- Part2/Gui.java
+++ Part2/Gui.java
@@ -17,26 +17,29 @@ public class Gui {
         frame.setSize(1200, 500);
         frame.setLayout(null);
         frame.setVisible(true);
+        frame.setResizable(false);
     
         trackPanel = new JPanel();
 
 
         trackPanel.setBackground(new Color(20, 20, 20)); 
-        trackPanel.setSize(800, 400); 
-        trackPanel.setLocation(350, 50);
+        trackPanel.setSize(800, 400);
+        trackPanel.setLocation(350,25);
         trackPanel.setLayout(null); 
         frame.add(trackPanel);
         frame.setVisible(true);
 
         startButton = new JButton("start Race");
         startButton.setSize(200, 30);
-        startButton.setLocation(500, 450);
+        startButton.setLocation(500, 425);
         frame.add(startButton);
+
 
         startButton.addActionListener(e -> {
         System.out.println("started race");
         });
     }
+
     public static void main(String[] args) {
             new Gui();
         }
