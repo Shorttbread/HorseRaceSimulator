d27ff9a7f10d6a9d1cf2d6168fb0383d3dc0bb38
Shorttbread <luisoliver6@gmail.com>
2025-04-24 03:43:12 +0100

    added horse icons to be displayed in the gui

--- Part2/Gui.java
+++ Part2/Gui.java
@@ -12,15 +12,14 @@ public class Gui {
 
         frame = new JFrame("Horse Race");
 
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1200, 500);
         frame.setLayout(null);
         frame.setVisible(true);
         frame.setResizable(false);
     
-        trackPanel = new JPanel();
 
+        trackPanel = new JPanel();
 
         trackPanel.setBackground(new Color(20, 20, 20)); 
         trackPanel.setSize(800, 400);
@@ -29,6 +28,7 @@ public class Gui {
         frame.add(trackPanel);
         frame.setVisible(true);
 
+        
         startButton = new JButton("start Race");
         startButton.setSize(200, 30);
         startButton.setLocation(500, 425);
@@ -37,7 +37,32 @@ public class Gui {
 
         startButton.addActionListener(e -> {
         System.out.println("started race");
+
+        ImageIcon horse1Icon = new ImageIcon("horse1.png");
+        ImageIcon horse2Icon = new ImageIcon("horse2.png");
+        ImageIcon horse3Icon = new ImageIcon("horse3.png");
+
+        horse1Icon = new ImageIcon(horse1Icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
+        horse2Icon = new ImageIcon(horse2Icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
+        horse3Icon = new ImageIcon(horse3Icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
+        
+        JLabel horse1Label = new JLabel(horse1Icon);
+        JLabel horse2Label = new JLabel(horse2Icon);
+        JLabel horse3Label = new JLabel(horse3Icon);
+        
+        int centerX = (trackPanel.getWidth() - 100) / 2;
+        horse1Label.setBounds(centerX, 30, 100, 100);
+        horse2Label.setBounds(centerX, 140, 100, 100);
+        horse3Label.setBounds(centerX, 250, 100, 100);
+        
+        trackPanel.add(horse1Label);
+        trackPanel.add(horse2Label);
+        trackPanel.add(horse3Label);
+
+        trackPanel.repaint();
+
         });
+            
     }
 
     public static void main(String[] args) {
